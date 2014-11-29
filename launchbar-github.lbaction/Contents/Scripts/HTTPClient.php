Duhemm/launchbar-github

commit da96522feda0d42817b11360f43c3c8e83f2f54a

Provide mean to update database schema
Running the action with `!update` will now update the database
schema. The token used for authentication will __NOT__ be lost.

launchbar-github.lbaction/Contents/Scripts/HTTPClient.php
@@ -11,6 +11,19 @@ class HTTPClient {
 	private $hostname = "https://api.github.com";
 	private $token = "";
 
+	private $dbSchema = "
+		CREATE TABLE settings (
+			'TOKEN' VARCHAR(255)
+		);
+		CREATE TABLE cache (
+			'ID' INTEGER PRIMARY KEY AUTOINCREMENT,
+			'URL' VARCHAR(255) UNIQUE,
+			'HASMORE' VARCHAR(255) UNIQUE,
+			'RESULTS' LONGTEXT,
+			'INSERTED' INTEGER
+		)
+	";
+
 	public static function getInstance() {
 		if(HTTPClient::$instance == null) {
 			HTTPClient::$instance = new HTTPClient();
@@ -134,6 +147,15 @@ class HTTPClient {
 		$this->pdo->prepare("DELETE FROM cache")->execute();
 	}
 
+	public function updateDB() {
+		$this->pdo->exec("DROP TABLE settings");
+		$this->pdo->exec("DROP TABLE cache");
+
+		$this->pdo->exec($this->dbSchema);
+
+		$this->updateToken($this->token);
+	}
+
 	private function __construct() {
 
 		$this->cookies = $_SERVER['LB_CACHE_PATH'] . '/cookies';
@@ -151,19 +173,7 @@ class HTTPClient {
 
 		// If the database did not exist, create the tables
 		if(!$existed) {
-			$this->pdo->exec("
-				CREATE TABLE settings (
-					'TOKEN' VARCHAR(255)
-				);
-
-				CREATE TABLE cache (
-					'ID' INTEGER PRIMARY KEY AUTOINCREMENT,
-					'URL' VARCHAR(255) UNIQUE,
-					'HASMORE' VARCHAR(255) UNIQUE,
-					'RESULTS' LONGTEXT,
-					'INSERTED' INTEGER
-				)
-			");
+			$this->pdo->exec($this->dbSchema);
 
 			chmod($dbPath, 0600); // The DB will contain the access token, don't let others read it !
 		} else {

launchbar-github.lbaction/Contents/Scripts/default.php
@@ -200,6 +200,14 @@ if($arg == NULL || !isset($arg->act)) {
 						));
 						break;
 
+					case 'update':
+						HttpClient::getInstance()->updateDB();
+
+						echo json_encode(array(
+							'title' => 'The database has been updated.'
+						));
+						break;
+
 					default:
 						echo json_encode(array(
 							'title' => 'Unknown action'

commit a7cde5dee6c13839f21f7a3b8837e74224b0dbd2

Fix broken cache
Commit da96522 introduced a mean update the database directly from
LaunchBar. Unfortunately, the schema that was present in the app
contained a small error that prevented the cache from working correctly.

This commit fixes the schema.

Fixes #2.

launchbar-github.lbaction/Contents/Scripts/HTTPClient.php
@@ -18,7 +18,7 @@ class HTTPClient {
 		CREATE TABLE cache (
 			'ID' INTEGER PRIMARY KEY AUTOINCREMENT,
 			'URL' VARCHAR(255) UNIQUE,
-			'HASMORE' VARCHAR(255) UNIQUE,
+			'HASMORE' VARCHAR(255),
 			'RESULTS' LONGTEXT,
 			'INSERTED' INTEGER
 		)